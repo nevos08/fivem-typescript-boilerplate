1c97f62c1a3639971a39ade527609b2036e4e486
Nevos <86106048+nevos08@users.noreply.github.com>
2023-05-10 10:20:02 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version "cerulean"
+game "gta5"
+
+author "Le0n"
+version "1.0.0"
+
+files {
+    "config.json"
+}
+
+client_scripts {
+    "dist/client.js"
+}
+
+server_scripts {
+    "dist/server.js"
+}
